b272fc62859c75f46466598e65b036b3a89a8cb8
Artem Yershov <ershov041091@icloud.com>
2023-07-21 19:10:51 +0300

    lesson's stuff

---
+++ LoginScreenApp/LoginScreenApp/SecondViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.swift
+//  LoginScreenApp
+//
+//  Created by Artem Yershov on 19.07.2023.
+//
+
+import Foundation
+import UIKit
+
+final class SecondViewController: UIViewController {
+    
+}
--- LoginScreenApp/LoginScreenApp/ViewController.swift
+++ LoginScreenApp/LoginScreenApp/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var emailTextFieldLine: UIView!
+    @IBOutlet weak var passwardTextField: UITextField!
+    @IBOutlet weak var passwardTextFieldLine: UIView!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var questionLable: UILabel!
+    @IBOutlet weak var signUpLable: UIButton!
+    
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configLoginButton()
     }
-
-
+    
+    //MARK: - Actions
+    
+    @IBAction func loginAction(_ sender: Any) {
+    }
+    
+    @IBAction func signUpAction(_ sender: Any) {
+    }
+    
+    private func configLoginButton() {
+        loginButton.layer.cornerRadius = 12
+        loginButton.layer.shadowOffset = .init(width: 3, height: 7)
+        loginButton.layer.shadowColor = UIColor.black.cgColor
+        loginButton.layer.cornerRadius = 5
+        loginButton.layer.shadowOpacity = 0.5
+    }
 }
 
